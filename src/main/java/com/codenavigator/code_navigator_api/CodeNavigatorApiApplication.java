8fda3eda90d35ff7f356fcbfc90bb03f4855a71f
Valdemir Trindade <vtrindade@MacBook-Air-de-Valdemir.local>
2025-05-11 12:32:25 -0300

    codigo inicial do projeto

---
+++ src/main/java/com/codenavigator/code_navigator_api/CodeNavigatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.codenavigator.code_navigator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNavigatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNavigatorApiApplication.class, args);
+	}
+
+}
